d2d6020ef2975f22428400fc060de900883c1a7e
Dennis-webdev <dennisrichter.business@gmail.com>
2020-05-03 17:10:40 +0200

    Updates

---
+++ precache-manifest.a9249c7d0ae2a8d6cb57172c62110265.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8740504f6968bc68e59637f2e3fe3a62",
+    "url": "/Snake/index.html"
+  },
+  {
+    "revision": "5e7f372fb42392eb21ab",
+    "url": "/Snake/static/css/main.17251b2f.chunk.css"
+  },
+  {
+    "revision": "05dbd7e6d5b658cb5a5a",
+    "url": "/Snake/static/js/2.f6d3a443.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Snake/static/js/2.f6d3a443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7f372fb42392eb21ab",
+    "url": "/Snake/static/js/main.527576c8.chunk.js"
+  },
+  {
+    "revision": "2b26981493a47a62dde3",
+    "url": "/Snake/static/js/runtime-main.4ca1da7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Snake/static/media/logo.5d5d9eef.svg"
+  }
+]);
